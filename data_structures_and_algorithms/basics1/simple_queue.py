b7b7646292a72ceb2edc8f29427907d1be71fffa
samirkhanal35 <samirkhanal35@gmail.com>
2021-06-18 17:18:32 +0545

    simple queue

---
+++ data_structures_and_algorithms/basics1/simple_queue.py
@@ -0,0 +1,91 @@
+'''
+It follows the FIFO rule.
+In programming terms, putting items in the queue is called enqueue, 
+and removing items from the queue is called dequeue.
+
+Basic Operations of Queue
+A queue is an object (an abstract data structure - ADT) that allows the following operations:
+
+Enqueue: Add an element to the end of the queue
+Dequeue: Remove an element from the front of the queue
+IsEmpty: Check if the queue is empty
+IsFull: Check if the queue is full
+Peek: Get the value of the front of the queue without removing it
+
+Working of Queue
+Queue operations work as follows:
+
+two pointers FRONT and REAR
+FRONT track the first element of the queue
+REAR track the last element of the queue
+initially, set value of FRONT and REAR to -1
+
+Enqueue Operation
+check if the queue is full
+for the first element, set the value of FRONT to 0
+increase the REAR index by 1
+add the new element in the position pointed to by REAR
+
+Dequeue Operation
+check if the queue is empty
+return the value pointed by FRONT
+increase the FRONT index by 1
+for the last element, reset the values of FRONT and REAR to -1
+
+The complexity of enqueue and dequeue operations in a queue using an array is O(1). 
+If you use pop(N) in python code, then the complexity might be O(n) depending on the 
+position of the item to be popped.
+
+Applications of Queue
+CPU scheduling, Disk Scheduling
+When data is transferred asynchronously between two processes.The queue is used for synchronization. For example: IO Buffers, pipes, file IO, etc
+Handling of interrupts in real-time systems.
+Call Center phone systems use Queues to hold people calling them in order.
+
+There are four different types of queues:
+
+Simple Queue
+Circular Queue
+Priority Queue
+Double Ended Queue
+
+'''
+# Queue implementation in Python
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+q.display()
+
+q.dequeue()
+
+print("After removing an element")
+q.display()
